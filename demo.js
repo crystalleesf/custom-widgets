04db8f2c99ca8d43855ce355d1147aca1d59bfb9
Crystal Lee <crystalleesf@gmail.com>
2021-05-16 16:50:30 -0700

    Completed widget toolkit

--- demo.js
+++ demo.js
@@ -2,67 +2,101 @@ import {Toolkit} from './toolkit.js';
 
 // Button
 var btn = new Toolkit.Button;
-btn.setPosition(100,100);
-btn.onClick(function(e){
-	console.log("Button has been clicked");
+btn.setBtnPosition(100,100);
+btn.onBtnClick(function(e){
+	
 });
-btn.onMouseOver(function(e) {
-	console.log("Hovering over button");
-})
-btn.setBtnLabel("Click Me!");
+btn.onBtnMouseOver(function(e) {
+	
+});
+btn.setBtnWidth(100);
+btn.setBtnLabel("Button");
+
 
 // Checkbox
 var checkbox = new Toolkit.Checkbox;
-checkbox.setPosition(100,200);
-checkbox.onClick(function(e){
-	console.log(e);
+checkbox.setCheckboxPosition(100,200);
+checkbox.onCheckboxClick(function(e){
+	let isChecked = checkbox.getCheckboxStatus() ? "checked" : "not checked";
+    console.log("Checkbox is " + isChecked);
 });
-checkbox.onMouseOver(function(e) {
-	console.log("Hovering over checkbox");
-})
+checkbox.onCheckboxMouseOver(function(e) {
+	
+});
 checkbox.setCheckboxLabel("Enrolled in INF 134");
+
 
 // Radio Buttons
 var radioButtons = new Toolkit.RadioButton;
-radioButtons.setPosition(100,300);
-radioButtons.setButtons(5, ["Radio 1", "Radio 2", "Radio 3", "Radio 4", "Radio 5"])
-radioButtons.onClick(function(e){
-	console.log(e);
+radioButtons.setRadioPosition(100,300);
+radioButtons.setRadioButtons(5, ["Radio 1", "Radio 2", "Radio 3", "Radio 4", "Radio 5"])
+radioButtons.onRadioClick(function(e){
+	
 });
-radioButtons.onHover(function(e){
-	console.log(e)
+radioButtons.onRadioHover(function(e){
+	
 });
+
 
 // Textbox
 var textbox = new Toolkit.Textbox;
-textbox.setPosition(600,200);
-textbox.setText("Textbox");
-textbox.setWidth(300);
-textbox.onTextChange(function() {
-	console.log("The text has changed in the textbox");
+textbox.setTextboxPosition(600,100);
+textbox.setTextboxText("Hello World");
+textbox.setTextboxWidth(300);
+textbox.onTextboxTextChange(function(e) {
+	
 });
+textbox.onTextboxHover(function(e) {
+	
+});
+
 
 // Scroll Bar
 var scrollbar = new Toolkit.Scrollbar;
+scrollbar.setScrollbarPosition(600,300);
+scrollbar.setScrollbarHeight(300);
+var pos = scrollbar.getThumbPosition();
+console.log("Thumb Position: " + pos);
+scrollbar.onScroll(function(e) {
+	
+});
+scrollbar.onScrollbarHover(function(e) {
+	
+});
+scrollbar.onScrollbarMouseUp(function(e) {
+	
+});
+scrollbar.onScrollbarMouseDown(function(e) {
+	
+});
 
 
 // Progress Bar
 var progressBar = new Toolkit.ProgressBar;
-progressBar.setPosition(600,300);
-progressBar.setWidth(400);
+progressBar.setProgressBarPosition(600,200);
+progressBar.setProgressBarWidth(400);
 progressBar.setIncrementValue(70);
 var incrementValue = progressBar.getIncrementValue();
-console.log("Initial Increment Value: " + incrementValue);
-progressBar.onIncrement(function() {
-	console.log("The progress bar has been incremented");
+console.log("Progress bar's initial increment value: " + incrementValue);
+progressBar.onIncrement(function(e) {
+	
 });
-progressBar.onDecrement(function() {
-	console.log("The progress bar has been decremented");
+progressBar.onDecrement(function(e) {
+	
 });
 progressBar.setIncrementDecrement(30);
+progressBar.onProgressBarMouseOver(function(e) {
+	
+});
 
 
-// Custom: Tooltip
-var tooltip = new Toolkit.Tooltip(textbox);
-tooltip.setPosition(600, 450);
-tooltip.setText("This is a tooltip");
+// Toggle Switch
+var toggleSwitch = new Toolkit.ToggleSwitch();
+toggleSwitch.setTogglePosition(700, 300);
+toggleSwitch.onToggleClick(function(e) {
+	let isOn = toggleSwitch.getToggleStatus() ? "on" : "off";
+    console.log("Toggle Switch is " + isOn);
+});
+toggleSwitch.onToggleHover(function(e) {
+
+});
